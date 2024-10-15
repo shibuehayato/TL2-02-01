1aa7f136c307b181ecb1e1c6fa8d6d2468901575
shibuehayato <115055046+shibuehayato@users.noreply.github.com>
2024-10-15 15:18:55 +0900

    02_04完了

--- main.cpp
+++ main.cpp
@@ -1,14 +1,33 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cassert>
+#include "TextureConverter.h"
+#include <combaseapi.h>
+
+// コマンドライン引数
+enum Argument {
+	kApplicationPath,  // アプリケーションのパス
+	kFilePath,		   // 渡されたファイルのパス
+
+	NumArgument
+};
 
 int main(int argc, char* argv[])
 {
-	for (int i = 0; i < argc; i++)
-	{
-		printf(argv[i]);
-		printf("\n");
-	}
-	printf("Hello,World!\n");
+	assert(argc >= NumArgument);
+
+	// COM ライブラリの初期化
+	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	assert(SUCCEEDED(hr));
+
+	// テクスチャコンバーター
+	TextureConverter converter;
+
+	// テクスチャ変換
+	converter.ConvertTextureWICToDDS(argv[kFilePath]);
+
+	// COM ライブラリの終了
+	CoUninitialize();
 
 	system("pause");
 	return 0;
